9473cc0351846971755d802c036844922f1502c6
FabrizioE42 <90227429+FabrizioE42@users.noreply.github.com>
2022-03-20 20:09:57 -0500

    Update telephoneNumberChecker.js

--- telephoneNumberChecker.js
+++ telephoneNumberChecker.js
@@ -1,7 +1,7 @@
 //This function checks if the input is a valid US phone number format including a 1 area code extension
 
 function telephoneCheck(str) {
-
+    // array contains different regex filters to check for all possible valid inputs
     const patterns = [
         //555-555-5555
         /^\d{3}-\d{3}-\d{4}$/,
@@ -22,7 +22,7 @@ function telephoneCheck(str) {
         //1(555)555-5555
         /^1\(\d{3}\)\d{3}-\d{4}$/];
 
-    return patterns.some((elem) => elem.test(str));
+    return patterns.some((elem) => elem.test(str)); // patterns.some will use each element in array to check if the string matches correct format
   }
   
   telephoneCheck("555-555-5555");
