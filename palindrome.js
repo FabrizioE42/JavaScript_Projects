669bc728d04fc9d191b4ac132da80c3e1121864c
FabrizioE42 <90227429+FabrizioE42@users.noreply.github.com>
2022-03-22 21:54:53 -0500

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,13 +1,13 @@
 function palindrome(str) {
-    function specialChar() { // removes all non-alphanumeric characters and converts to lowercase
+    function specialChar() { // This function removes all non-alphanumeric characters and converts to lowercase
 
-        const regex = /[^A-Za-z0-9]/g;
-        const newstr = str.replace(regex, "").toLowerCase();
+        const regex = /[^A-Za-z0-9]/g; // regular expression
+        const newstr = str.replace(regex, "").toLowerCase(); //if it finds non-alphanumeroc it replaces with nothing while converting to lowercase
         return newstr;
     }
-    let filteredstr = specialChar();
-    let reverse = filteredstr.split("").reverse().join("");
-    if(filteredstr==reverse) {
+    let filteredstr = specialChar(); // call function
+    let reverse = filteredstr.split("").reverse().join(""); // convert string to array, then reverse its order, and convert back to string
+    if(filteredstr==reverse) { // check if the reverse string is the same as original
         return true;
     }
     return false;
